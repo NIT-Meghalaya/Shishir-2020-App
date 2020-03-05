d627367d2a3f9bb7008b38c3f7887567a7796f33
Devansh-Maurya <devansh233@gmail.com>
2020-03-05 23:34:35 +0530

    Added a navigation graph

---
+++ app/src/main/java/nitmeghalaya/shishir2020/screens/eventslist/EventsListViewModel.kt
@@ -0,0 +1,10 @@
+package nitmeghalaya.shishir2020.screens.eventslist
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Devansh on 5/3/20
+ */
+
+class EventsListViewModel: ViewModel() {
+}
