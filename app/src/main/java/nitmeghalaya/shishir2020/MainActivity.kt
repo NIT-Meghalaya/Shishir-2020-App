783c28852d021b5ab9997a8598a82948e9288c9f
Devansh-Maurya <devansh233@gmail.com>
2020-03-06 00:55:56 +0530

    Added EventsScheduleFragment

--- app/src/main/java/nitmeghalaya/shishir2020/MainActivity.kt
+++ app/src/main/java/nitmeghalaya/shishir2020/MainActivity.kt
@@ -2,11 +2,21 @@ package nitmeghalaya.shishir2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.navHostFragment)
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController)
     }
 }
---
+++ app/src/main/java/nitmeghalaya/shishir2020/screens/eventsschedule/EventsScheduleViewModel.kt
@@ -0,0 +1,10 @@
+package nitmeghalaya.shishir2020.screens.eventsschedule
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Devansh on 6/3/20
+ */
+
+class EventsScheduleViewModel: ViewModel() {
+}
