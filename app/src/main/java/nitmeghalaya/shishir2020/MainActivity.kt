4d095a0e72e5bf0b0be73ab3b77cf3c9458148d5
Devansh-Maurya <devansh233@gmail.com>
2020-03-06 01:33:56 +0530

    Added fragments as top level fragments to prevent showing 'up' button

--- app/src/main/java/nitmeghalaya/shishir2020/MainActivity.kt
+++ app/src/main/java/nitmeghalaya/shishir2020/MainActivity.kt
@@ -2,7 +2,10 @@ package nitmeghalaya.shishir2020
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,6 +20,9 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.navHostFragment)
         bottomNavigationView.setupWithNavController(navController)
-        setupActionBarWithNavController(navController)
+
+        //This prevents showing of up button on toolbar for fragments at top level
+        val appBarConfiguration = AppBarConfiguration(bottomNavigationView.menu)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
