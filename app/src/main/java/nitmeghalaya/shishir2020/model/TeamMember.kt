644ebe09324b0cb296c465105d5699ac5f8b87f3
Pankaj <golupankajgoyal2@gmail.com>
2020-03-07 12:24:17 +0530

    Added model directory and BaseFragment.kt with some changes in activity_main.xml

---
+++ app/src/main/java/nitmeghalaya/shishir2020/model/TeamMember.kt
@@ -0,0 +1,10 @@
+package nitmeghalaya.shishir2020.model
+
+data class TeamMember(
+    val name: String = "",
+    val post: String = "",
+    val email: String = "",
+    val contact: String = "",
+    val team: String = "",
+    val imageResId: Int = 0
+)
