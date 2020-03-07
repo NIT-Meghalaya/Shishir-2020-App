d7c637e007718e6741c2082204228efaa77e3e96
Devansh-Maurya <devansh233@gmail.com>
2020-03-07 15:50:50 +0530

    Now getting Facebook Access Token to access Shishir Page. The token creator was stored earlier in the Firestore database. It is then being used to create the access token.

---
+++ app/src/main/java/nitmeghalaya/shishir2020/di/KoinModule.kt
@@ -0,0 +1,18 @@
+package nitmeghalaya.shishir2020.di
+
+import nitmeghalaya.shishir2020.repository.FirestoreRepository
+import nitmeghalaya.shishir2020.screens.shishirpagefeed.ShishirPageFeedViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+/**
+ * Created by Devansh on 7/3/20
+ */
+
+val firestoreRepositoryModule = module {
+    single { FirestoreRepository() }
+}
+
+val appModule = module {
+    viewModel { ShishirPageFeedViewModel(get()) }
+}
--- app/src/main/java/nitmeghalaya/shishir2020/screens/shishirpagefeed/ShishirPageFeedFragment.kt
+++ app/src/main/java/nitmeghalaya/shishir2020/screens/shishirpagefeed/ShishirPageFeedFragment.kt
@@ -5,16 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import nitmeghalaya.shishir2020.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 class ShishirPageFeedFragment : Fragment() {
+
+    private val shishirPageFeedViewModel: ShishirPageFeedViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shishir_page_feed, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        shishirPageFeedViewModel.getFacebookAccessToken().observe(viewLifecycleOwner, Observer {
+            Timber.i(it.toString())
+        })
+    }
 }
