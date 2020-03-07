9e43f3a2c353f01e18afaae87e0c85f1319bc449
Devansh-Maurya <devansh233@gmail.com>
2020-03-07 18:04:22 +0530

    Added code to fetch events data from Firestore

--- app/src/main/java/nitmeghalaya/shishir2020/screens/eventslist/EventsListViewModel.kt
+++ app/src/main/java/nitmeghalaya/shishir2020/screens/eventslist/EventsListViewModel.kt
@@ -1,10 +1,30 @@
 package nitmeghalaya.shishir2020.screens.eventslist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.toObjects
+import nitmeghalaya.shishir2020.model.ShishirEvent
+import nitmeghalaya.shishir2020.repository.FirestoreRepository
+import timber.log.Timber
 
 /**
  * Created by Devansh on 5/3/20
  */
 
-class EventsListViewModel: ViewModel() {
+class EventsListViewModel(private val firestoreRepository: FirestoreRepository): ViewModel() {
+
+    fun getAllEventsList(): LiveData<List<ShishirEvent>> {
+        val eventsListLiveData = MutableLiveData<List<ShishirEvent>>()
+
+        firestoreRepository.getAllEventsList().addOnSuccessListener {
+            val eventsList = it.toObjects<ShishirEvent>()
+            eventsListLiveData.value = eventsList
+        }.addOnFailureListener {
+            Timber.e("Failed to get events list")
+        }
+
+        return eventsListLiveData
+    }
+
 }
