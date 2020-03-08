83f70c6e19e3817c93632e9c9039a028e4b89fb9
Devansh-Maurya <devansh233@gmail.com>
2020-03-08 23:40:38 +0530

    Added Coil image loader library. Removed moshi-kotlin. The moshi-kotlin library used reflection and was big in size. Codegen serves similar purpose

--- app/src/main/java/nitmeghalaya/shishir2020/network/RetrofitService.kt
+++ app/src/main/java/nitmeghalaya/shishir2020/network/RetrofitService.kt
@@ -1,7 +1,5 @@
 package nitmeghalaya.shishir2020.network
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -13,13 +11,9 @@ private const val BASE_URL = "https://graph.facebook.com/v6.0/347129762068773/"
 
 object RetrofitService {
 
-    private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
-
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
     fun <T> createService(serviceClass: Class<T>): T = retrofit.create(serviceClass)
