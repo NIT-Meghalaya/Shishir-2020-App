1b40e94ea223cbc2affcfa02340e450245cd3741
Devansh-Maurya <devansh233@gmail.com>
2020-03-08 23:56:07 +0530

    Now also showing feed item images in the feed. The image view adjusts according to the image using the adjustViewBounds attribute

--- app/src/main/java/nitmeghalaya/shishir2020/screens/facebookpagefeed/FacebookPageFeedViewHolder.kt
+++ app/src/main/java/nitmeghalaya/shishir2020/screens/facebookpagefeed/FacebookPageFeedViewHolder.kt
@@ -1,6 +1,7 @@
 package nitmeghalaya.shishir2020.screens.facebookpagefeed
 
 import android.view.View
+import coil.api.load
 import kotlinx.android.synthetic.main.item_facebook_page_feed.view.*
 import nitmeghalaya.shishir2020.model.facebookpagefeed.FacebookPageFeedItem
 import nitmeghalaya.shishir2020.screens.BaseViewHolder
@@ -12,6 +13,15 @@ import nitmeghalaya.shishir2020.screens.BaseViewHolder
 class FacebookPageFeedViewHolder(itemView: View): BaseViewHolder<FacebookPageFeedItem>(itemView) {
 
     override fun bind(item: FacebookPageFeedItem) {
-        itemView.messageTV.text = item.message
+        itemView.apply {
+            messageTV.text = item.message
+
+            if (item.fullPictureUrl.isNotEmpty()) {
+                feedItemImage.visibility = View.VISIBLE
+                feedItemImage.load(item.fullPictureUrl)
+            } else {
+                feedItemImage.visibility = View.GONE
+            }
+        }
     }
 }
