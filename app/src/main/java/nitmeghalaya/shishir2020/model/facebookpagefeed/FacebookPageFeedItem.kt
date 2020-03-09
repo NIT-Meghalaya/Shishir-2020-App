7861debb8ad80a69e9a2533de1e933b65b09bab1
Devansh-Maurya <devansh233@gmail.com>
2020-03-09 23:56:38 +0530

    Added hashcode and equals method to feed item class. Working on pagination

--- app/src/main/java/nitmeghalaya/shishir2020/model/facebookpagefeed/FacebookPageFeedItem.kt
+++ app/src/main/java/nitmeghalaya/shishir2020/model/facebookpagefeed/FacebookPageFeedItem.kt
@@ -18,4 +18,28 @@ class FacebookPageFeedItem(
     companion object {
         fun getAllProperties(): String = "id,message,full_picture,created_time"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FacebookPageFeedItem
+
+        if (id != other.id) return false
+        if (message != other.message) return false
+        if (fullPictureUrl != other.fullPictureUrl) return false
+        if (createdTime != other.createdTime) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + message.hashCode()
+        result = 31 * result + fullPictureUrl.hashCode()
+        result = 31 * result + createdTime.hashCode()
+        return result
+    }
+
+
 }
