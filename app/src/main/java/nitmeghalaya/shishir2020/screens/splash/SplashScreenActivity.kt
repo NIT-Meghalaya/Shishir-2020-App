db8ceb015dc3c22f9914ad9ac50ac70799090ebd
Pankaj <golupankajgoyal2@gmail.com>
2020-03-14 22:35:29 +0530

    Added Introduction Slides

--- app/src/main/java/nitmeghalaya/shishir2020/screens/splash/SplashScreenActivity.kt
+++ app/src/main/java/nitmeghalaya/shishir2020/screens/splash/SplashScreenActivity.kt
@@ -3,10 +3,11 @@ package nitmeghalaya.shishir2020.screens.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.WindowManager
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
 import nitmeghalaya.shishir2020.R
 import nitmeghalaya.shishir2020.screens.MainActivity
+import nitmeghalaya.shishir2020.screens.introslider.IntroSliderActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -15,11 +16,20 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+//        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        val getPrefs = PreferenceManager
+            .getDefaultSharedPreferences(baseContext)
+        val isFirstStart = getPrefs.getBoolean("firstStart", true)
 
         Handler().postDelayed({
-            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+//            startActivity(intent)
+
+            if (isFirstStart) {
+                startActivity(Intent(this, IntroSliderActivity::class.java))
+            } else {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
 
             //Close this activity
             finish()
